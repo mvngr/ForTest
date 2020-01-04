4681096b8d6780371c7c08cdc7958fc01aaf056e
Mike <mvngr@niuitmo.ru>
2020-01-04 13:04:00 +0300

    Начал работу над красно-черным деревом

--- main.cpp
+++ main.cpp
@@ -6,12 +6,14 @@
 #include <iterator>
 #include <initializer_list>
 
+/*!
+ * \brief Неизменяемая строка
+ */
 class StringView
 {
 public:
-    //constexpr StringView(const char *inString) : str_(inString), length_(sizeof(inString) / sizeof(char)) { }
     template<std::size_t N>
-    constexpr StringView(const char(&inString)[N]) noexcept : str_{inString}, length_{N - 1} {}
+    constexpr StringView(const char(&inString)[N]) : str_{inString}, length_{N - 1} {}
 
     constexpr std::size_t size() const
     {
@@ -62,15 +64,58 @@ private:
     }
 };
 
+/*!
+ *  \brief неймспейс для всех классов, относящихся к дереву
+ */
+namespace Tree
+{
+    /*!
+     * \brief Перечисление для удобной работы с красным и черным типом через boolean
+     */
+    enum Colors{
+        RED = false,
+        BLACK
+    };
 
+    /*!
+     * \brief Нода для красно-черного дерева
+     */
+    template<typename T>
+    class Node
+    {
+    public:
+        constexpr Node(T inData) :
+            data_(inData),
+            color_(Colors::RED),
+            left_(nullptr),
+            right_(nullptr),
+            parent_(nullptr)
+        {}
+    private:
+        T data_;
+        bool color_;
+        Node<T> *left_, *right_, *parent_;
+    };
+
+    /*!
+     * \brief Красно-черное дерево
+     */
+    template<typename T>
+    class RBTree
+    {
+
+    };
+}
 
 int main()
 {
     constexpr StringView s ("sss");
     constexpr StringView a ("ss2s");
 
+    //тест корректного наполнения и сравнения классов
     std::cout << s.size() << " " << a.size() << " " << (s == a ? "true" : "false") << std::endl;
 
+    constexpr Tree::Node<StringView> node("sssss"); //сейчас переменная только для теста инициализации
 
 
     return 0;
